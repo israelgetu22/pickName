30224dd56648a300b52b42059d2dd46690a879b2
Isarel Getu <israelgetu101@gmail.com>
2023-01-12 11:56:55 -0500

    done

--- jsPickName.js
+++ jsPickName.js
@@ -1,14 +1,51 @@
 console.log("Practice interview Questions");
 
 const users = [
-  { id: 1, name: "Jack", inActive: true },
-  { id: 2, name: "John", inActive: true },
-  { id: 3, name: "Mike", inActive: false },
+  { id: 1, name: "Jack", isActive: true, age: 20 },
+  { id: 2, name: "John", isActive: true, age: 18 },
+  { id: 3, name: "Mike", isActive: false, age: 30 },
 ];
 
-const names = [];
+const names = users
+  .sort((user1, user2) => (user1.age < user2.age ? 1 : -1))
+  .filter((user) => user.isActive)
+  .map((user) => user.name);
+
+console.log(names);
+
+// users.sort((user1, user2) => (user1.age < user2.age ? -1 : 1));
+
+// const names = [];
+// for (let i = 0; i < users.length; i++) {
+//   if (users[i].isActive) {
+//     names.push(users[i].name);
+//   }
+// }
+// console.log(names);
+
+/*const names = [];
 for (let i = 0; i < users.length; i++) {
   names.push(users[i].name);
 }
 
 console.log("names", names);
+
+
+const names = [];
+users.forEach((user) => {
+  names.push(user.name);
+});
+
+console.log(names);
+
+const names = users.map((user) => user.name);
+console.log(names);
+
+
+
+const names = users.filter((user) => user.isActive).map((user) => user.name);
+console.log(names);
+
+*/
+
+//console.log(names.reverse());
